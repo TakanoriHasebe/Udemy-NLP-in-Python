4b536aa13a16e7cb714260ef9cd6b5474a6cbd20
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-07-12 15:28:43 +0900

    article spinnerの追加

---
+++ article-spinner-python/current.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 14:59:19 2017
+
+@author: Takanori
+"""
+
+"""
+article-spinner-with-python
+"""
+
+import nltk
+import random
+import numpy as np
+
+from bs4 import BeautifulSoup
+
+# load the reviews
+# data courtesy of http://www.cs.jhu.edu/~mdredze/datasets/sentiment/index2.html
+positive_reviews = BeautifulSoup(open('electronics/positive.review').read())
+positive_reviews = positive_reviews.findAll('review_text')
+
+trigrams = {}
+for review in positive_reviews:
+    s = review.text.lower()
+    tokens = nltk.tokenize.word_tokenize(s)
+    for i in range(len(tokens) - 2):
+        k = (tokens[i], tokens[i+2])
+        if k not in trigrams:
+            trigrams[k] = []
+        trigrams[k].append(tokens[i+1])
+
+trigrams_probabilities = {}
+for k, words in trigrams.items():
+    if len(set(words)) > 1:
+        d = {}
+        n = 0
+        for w in words:
+            if w not in d:
+                d[w] = 0
+            d[w] += 1
+            n += 1
+        for w,c in d.items():
+            d[w] = float(c) / n
+        trigrams_probabilities[k] = d
+        
+def random_sample(d):
+    
+    r = random.random()
+    cumulative = 0
+    for w, p in d.items():
+        cumulative += p
+        if r < cumulative:
+            return w
+
+def test_spinner():
+    review = random.choice(positive_reviews)
+    s = review.text.lower()
+    print("Original:"+str(s))
+    tokens = nltk.tokenize.word_tokenize(s)
+    for i in range(len(tokens) - 2):
+        if random.random() < 0.2:
+            k = (tokens[i], tokens[i+2])
+            if k in trigrams_probabilities:
+                w = random_sample(trigrams_probabilities[k])
+                tokens[i+1] = w
+    print("Spun:")
+    print(" ".join(tokens).replace(" .", ".").replace(" '", "'").repalce(" ,", ",").repalce("$ ", "$").replace(" !", "!"))
+
+
